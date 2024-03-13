51ed0c87e5c0edeca1a503f27ebb78876bc5d52d
mactepluke <licensetospam@icloud.com>
2024-03-13 12:28:45 +0100

    Wrote FAQ

--- src/app/pricing/pricing-page/pricing-page.component.ts
+++ src/app/pricing/pricing-page/pricing-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'sycm-pricing-page',
   standalone: true,
-  imports: [],
+    imports: [
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        MatCardSubtitle,
+        MatCardTitle
+    ],
   templateUrl: './pricing-page.component.html',
   styleUrl: './pricing-page.component.css'
 })
