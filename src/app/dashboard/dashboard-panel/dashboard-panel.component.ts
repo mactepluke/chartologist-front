ea7e4062a3d4f65c204830a013ced47d0609c344
mactepluke <licensetospam@icloud.com>
2024-03-25 20:52:13 +0100

    Minor refactorings

--- src/app/dashboard/dashboard-panel/dashboard-panel.component.ts
+++ src/app/dashboard/dashboard-panel/dashboard-panel.component.ts
@@ -10,19 +10,23 @@ import {
 import {MatButton} from "@angular/material/button";
 import {AuthService} from "../../auth/services/auth.service";
 import {DisplayService} from "../../shared_services/display.service";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
     selector: 'sycm-dashboard-panel',
     standalone: true,
-    imports: [
-        MatCard,
-        MatCardTitle,
-        MatCardSubtitle,
-        MatCardHeader,
-        MatCardContent,
-        MatCardActions,
-        MatButton
-    ],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    ReactiveFormsModule,
+    MatSlideToggle
+  ],
     providers: [
         AuthService,
       DisplayService
@@ -34,8 +38,18 @@ export class DashboardPanelComponent {
   @Output()
   isLoggedIn : EventEmitter<boolean> = new EventEmitter<boolean>
 
-    constructor(private authService: AuthService, private displayService: DisplayService) {
+  formGroup = this.formBuilder.group({
+    enableWifi: '',
+    acceptTerms: ['', Validators.requiredTrue],
+  });
+
+
+  constructor(private authService: AuthService, private displayService: DisplayService, private formBuilder: FormBuilder) {
     }
+
+  alertFormValues(formGroup: FormGroup) {
+    alert(JSON.stringify(formGroup.value, null, 2));
+  }
 
     onLogout(): void {
         this.authService.logout();
